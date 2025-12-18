f68a1c18fe8ad70dd37266877958af0ddcfcc7df
Adarsh Hiwrale <adarsh.hiwrale@7pi.co>
2025-12-18 23:37:45 +0530

    cpu_options fix

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_subnet" "this" {
 
 data "aws_ec2_instance_type" "default" {
   count = (
-    var.cpu_options != null && var.cpu_options.threads_per_core != null && var.cpu_options.core_count == null ? 1 : 0
+    var.cpu_options != null && try(var.cpu_options.threads_per_core, null) != null && try(var.cpu_options.core_count, null) == null ? 1 : 0
   )
 
   instance_type = var.instance_type
@@ -152,8 +152,8 @@ resource "aws_launch_template" "default" {
 
 locals {
   launch_template_block = {
-    id      = join("", aws_launch_template.default.*.id)
-    version = var.launch_template_version != "" ? var.launch_template_version : join("", aws_launch_template.default.*.latest_version)
+    id      = one(aws_launch_template.default[*].id)
+    version = var.launch_template_version != "" ? var.launch_template_version : one(aws_launch_template.default[*].latest_version)
   }
   launch_template = (
     var.mixed_instances_policy == null ? local.launch_template_block
